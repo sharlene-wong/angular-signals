1c8e8ccedfacbff8b61e7331e87bb2a34fa219a7
Sharlene Wong <sharlenew@youi.com>
2026-05-29 14:43:57 +1000

    feat: reading computed values

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductData } from '../product-data';
 import { Product } from '../product';
@@ -17,6 +17,8 @@ export class ProductSelection {
   quantity = signal(1);
 
   products = signal(ProductData.products);
+
+  total = computed(() => (this.selectedProduct()?.price ?? 0) * this.quantity());
 
   onIncrease() {
     this.quantity.update(q => q + 1); // update receives the current value.  Use the arrow function to determine the new value, q = parameter, q + 1 = new value.  This is the recommended way to update a signal when the new value depends on the current value.
